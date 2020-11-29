95643d2ac779abfbdbebeff5cd6c8f6517677571
celia <22843@student.dorset-college.ie>
2020-11-29 18:47:57 +0000

    2020/11/29

---
+++ ApplicationForCourse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Group1_OOP
+{
+    public class ApplicationForCourse
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string Name { get; set; }
+        public int StudyYear { get; set; }
+        public string Class { get; set; }
+        public List<Course> Courses { get; set; }
+        public Timetable StudentTimetable { get; set; }
+        public Timetable ProfessorTimetable { get; set; }
+        public string Index_course { get; set; }
+
+        string Status { get; set; } //En attente, Acceptée, Refusée
+
+
+        public ApplicationForCourse(string id, string firstname, string name, int year, string _class, List<Course> courses, Timetable timetable, string index_course)
+        {
+            ID = id;
+            FirstName = firstname;
+            Name = name;
+            StudyYear = year;
+            Class = _class;
+            Courses = courses;
+            StudentTimetable = timetable;
+            Index_course = index_course;
+
+            //Ajouter Trouver nom du cours et du prof
+
+            Status = "Pending";
+        }
+
+        public void ShowApplicationForRegistration()
+        {
+            Console.WriteLine($"STUDENT APPLICATION FOR A COURSE \n\nID : {ID} \nFirstname : {FirstName} \nName : {Name} \nYear : {StudyYear} \n Class : {Class} \n\n\n");
+
+            StudentTimetable.ShowTimetable();
+            ProfessorTimetable.ShowTimetable();
+        }
+    }
+}
---
+++ CoursePlan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Group1_OOP
+{
+    public class CoursePlan
+    {
+        string S1week1 { get; set; }
+        string S1week2 { get; set; }
+        string S1week3 { get; set; }
+        string S1week4 { get; set; }
+        string S1week5 { get; set; }
+        string S1week6 { get; set; }
+        string S1week7 { get; set; }
+        string S1week8 { get; set; }
+        string S1week9 { get; set; }
+        string S1week10 { get; set; }
+        string S1week11 { get; set; }
+        string S1week12 { get; set; }
+
+        string S2week1 { get; set; }
+        string S2week2 { get; set; }
+        string S2week3 { get; set; }
+        string S2week4 { get; set; }
+        string S2week5 { get; set; }
+        string S2week6 { get; set; }
+        string S2week7 { get; set; }
+        string S2week8 { get; set; }
+        string S2week9 { get; set; }
+        string S2week10 { get; set; }
+        string S2week11 { get; set; }
+        string S2week12 { get; set; }
+
+        public CoursePlan()
+        {
+
+        }
+    }
+}
